3d5860cea49b0a25f6173017ac2c2d84164231d1
bogdaryan <bogdan10050@gmail.com>
2022-08-17 01:40:14 +0300

    first

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import '@styles/app.scss';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Template</h2>
+                <h1>React App</h1>
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const projectRoot = path.resolve(__dirname);
+
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.join(__dirname, "/dist"),
+        filename: "index_bundle.js"
+    },
+    resolve: {
+        alias: {
+            '@': projectRoot + '/src',
+            '@components': projectRoot + '/src/components',
+            '@styles': projectRoot + '/src/styles',
+            '@assets': projectRoot + '/assets',
+        },
+        extensions: [
+            '.js', '.json', '.css', '.scss', '.sass',
+            '.svg', '.png', '.jpeg', '.gif'
+        ],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.(scss|css)$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        limit: 512,
+                        quality: 85
+                    }
+                }]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.(mov|mp4)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html"
+        })
+    ]
+};
