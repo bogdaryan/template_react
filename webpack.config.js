3eda7fc5909718683f01780c61a9ac51fb7142d4
Bogdan <40521570+bogdaryan@users.noreply.github.com>
2022-09-25 10:42:33 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -37,25 +37,15 @@ module.exports = {
             },
             {
                 test: /\.(png|svg|jpg|gif)$/,
-                use: [{
-                    loader: 'file-loader',
-                    options: {
-                        limit: 512,
-                        quality: 85
-                    }
-                }]
+                type: 'asset/resource',
             },
             {
                 test: /\.(woff|woff2|eot|ttf|otf)$/,
-                use: [
-                    'file-loader'
-                ]
+                type: 'asset/resource',
             },
             {
                 test: /\.(mov|mp4)$/,
-                use: [
-                    'file-loader'
-                ]
+                type: 'asset/resource',
             },
         ]
     },
