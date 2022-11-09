1a3d205c8d6869352da39681cd8389fd65d89ba8
Bogdan <bogdan10050@gmail.com>
2022-11-09 17:28:56 +0300

    add mini-css-extract-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,60 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const projectRoot = path.resolve(__dirname);
 
 
 module.exports = {
-    entry: "./src/index.js",
-    output: {
-        path: path.join(__dirname, "/dist"),
-        filename: "index_bundle.js"
-    },
-    resolve: {
-        alias: {
-            '@': projectRoot + '/src',
-            '@components': projectRoot + '/src/components',
-            '@styles': projectRoot + '/src/styles',
-            '@assets': projectRoot + '/src/assets',
-        },
-        extensions: [
-            '.js', '.json', '.css', '.scss', '.sass',
-            '.svg', '.png', '.jpeg', '.gif'
-        ],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
-            },
-            {
-                test: /\.(scss|css)$/,
-                use: ['style-loader', 'css-loader', 'sass-loader'],
-            },
-            {
-                test: /\.(png|svg|jpg|gif)$/,
-                type: 'asset/resource',
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/,
-                type: 'asset/resource',
-            },
-            {
-                test: /\.(mov|mp4)$/,
-                type: 'asset/resource',
-            },
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./src/index.html"
-        })
-    ]
+	entry: "./src/index.js",
+	output: {
+		path: path.join(__dirname, "/dist"),
+		filename: "index_bundle.js"
+	},
+	resolve: {
+		alias: {
+			'@': projectRoot + '/src',
+			'@components': projectRoot + '/src/components',
+			'@styles': projectRoot + '/src/styles',
+			'@assets': projectRoot + '/src/assets',
+		},
+		extensions: [
+			'.js', '.json', '.css', '.scss', '.sass',
+			'.svg', '.png', '.jpeg', '.gif'
+		],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader"
+				}
+			},
+			{
+				test: /\.(scss|css)$/,
+				use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+			},
+			{
+				test: /\.(png|svg|jpg|gif)$/,
+				type: 'asset/resource',
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/,
+				type: 'asset/resource',
+			},
+			{
+				test: /\.(mov|mp4)$/,
+				type: 'asset/resource',
+			},
+		]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './src/index.html'
+		}),
+		new MiniCssExtractPlugin({
+			filename: '[name].css',
+		})
+	]
 };
